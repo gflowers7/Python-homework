bd4c4a798ef6ad21f55ac9aa138b6e40805e2754
gigi <gigi987@berkeley.edu>
2024-10-18 00:30:00 -0700

    removing this file bc it was wrong

--- extract_text.py
+++
@@ -1,3 +0,0 @@
-## 1. Oski Stole Your Power
-Oski hacked your computer and you can no longer use `x**y` or `pow(x, y)`. 
-Find a different way (by writing a function) that can compute `x` raised to the power of `y`.
